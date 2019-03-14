a5194d6ff32cb65a17771289527fa0c520bcf0d0
MIghtykukulkan <uma.s.shankar@gmail.com>
2019-03-14 18:17:42 +0530

    finished knn

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 const _ = require('lodash');
 var multer  = require('multer')
 var upload = multer({ dest: 'uploads/' })
-
+const PredictCost = require('./costknn');
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send("Welcome to Rapidpredict !"));
 
-app.post('/uploadscv', upload.any(), function (req, res) {
+app.post('/uploadcsv', upload.any(), function (req, res) {
 
     console.log(req.files[0])
 
@@ -30,18 +30,38 @@ app.post('/uploadscv', upload.any(), function (req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify({'status':'uploadSuccessful', 'path':req.files[0].path, 'headers':headers}));
 
-    /*
-    let { features, labels, testFeatures, testLabels } = loadCSV(req.files[0].path, {
-        shuffle: true,
-        splitTest: 50,
-        dataColumns: ['horsepower', 'weight', 'displacement'],
-        labelColumns: ['mpg']
-      });
-
-      console.log(features);
-      */
    
-})
+});
+
+app.post('/predict',  function (req, res) {
+
+  var body = req.body;
+  
+
+
+  let { features, labels, testFeatures, testLabels } = loadCSV(body.path, {
+    shuffle: true,
+    splitTest: 50,
+    dataColumns: ['horsepower', 'weight', 'displacement'],
+    labelColumns: ['mpg']
+  });
+
+    var options = {
+      "kvalue": 10,
+      "strategy":"OCC"
+    };
+    
+var predictionPoint = [90 , 1.5425   , 232 ];
+
+
+  var prediction = new PredictCost(features, labels, testFeatures, testLabels, predictionPoint, options);
+
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({'prediction':prediction.computeKnn(), "test_report":prediction.findAccuracy()}));
+
+
+});
+
 
 
 //**********************************/
--- costknn.js
+++ costknn.js
@@ -1,76 +1,73 @@
-
 /*{"_id":"5c6a83e1310dd83f24aa1fd9","item_description":"window","material":"glass","damage_percentage":40,"cost_repair":10000,"location":"Jaipur","image_url":"","claimid":""}
 
 // writing KNN classifcation problem for the prediction of the cost damages */
-
 require('@tensorflow/tfjs-node')
 const tf = require('@tensorflow/tfjs')
 var _ = require('lodash');
+const loadCSV = require('./load-csv');
 //-----------------------
 
 class PredictCost {
-  
-    constructor(dataSet, predictionPoint){
-        this.rawdata = dataSet;
-        this.predictionPoint = predictionPoint;
-        this.K = 2;
 
-        this.data_prep(this.rawdata)
+    typeConvert(data) {
+        var mainArray = []
+
+        _.forEach(data, (row) => {
+
+            var tempArray = [];
+            for (var i in row) {
+                if (typeof(row[i]) == String)
+                    tempArray.push(this.funhash(row[i]));
+                else
+                    tempArray.push(row[i]);
+            }
+            mainArray.push(tempArray);
+
+        })
+
+        return tf.tensor(mainArray);
     }
 
-   
-    findIdealK(){
-        //TODO
-    }
+    constructor(features, labels, testFeatures, testLabels, predictionPoint, options) {
+        this.features = this.typeConvert(features);
+        this.labels = tf.tensor(labels);
+        this.predictionPoint = tf.tensor(predictionPoint);
+        this.testFeatures = testFeatures;
+        this.testLabels = testLabels;
 
-    typeConvert(row){
-        var tempArray = []    ;
-            tempArray.push(this.funhash(row["item_description"]));
-            tempArray.push(this.funhash(row["material"]));
-            tempArray.push(row["damage_percentage"]);
-            tempArray.push(this.funhash(row["location"]));
-            return tempArray
+        this.options = Object.assign({
+                'kvalue': 3,
+                'strategy':"OCC"
+            },
+            options
+        );
     }
 
     funhash(s) {
-        for(var i = 0, h = 0xdeadbeef; i < s.length; i++)
+        for (var i = 0, h = 0xdeadbeef; i < s.length; i++)
             h = Math.imul(h ^ s.charCodeAt(i), 2654435761);
         return (h ^ h >>> 16) >>> 0;
     }
 
-
-    //preparing feature array and label array and preparing the Tensor 
-    data_prep(rawdata){
-        let MLFeatureData = [];
-        let MLlabelData = [];
-        this.typeConvert(rawdata[0])
-        _.forEach(rawdata, function(row){        
-            MLlabelData.push([row["cost_repair"]])
-            MLFeatureData.push(this.typeConvert(row))
-        }.bind(this))
-
-        this.features = tf.tensor(MLFeatureData);
-        this.labels = tf.tensor(MLlabelData)
-        this.predictionPoint = tf.tensor(this.typeConvert(this.predictionPoint))
-    }
-
-    splitDataSet(dataSet){
-        [testData, traingData] = dataSet.splice(0,3) ;
-        testData.forEach(row => {
-            findIdealK(row)
-        })
-    }
-
-    
-    printTensor(){
-        features.print();
-        labels.print();
-        predictionPoint.print()
+    printTensor() {
+        this.features.print();
+        this.labels.print();
+        this.predictionPoint.print();
     }
 
     // converts the string features to numeric values by hashing them
-    
-    
+    findAccuracy(){
+        // formula = actual - precited / actual
+        this.accuracyReport = [];
+        this.testFeatures.forEach((item, index)=>{
+            this.predictionPoint = item;
+            var predictedValue = this.computeKnn()
+            var accuracy = (this.testLabels[index][0] - predictedValue)/this.testLabels[index][0];
+            this.accuracyReport.push({"actual":this.testLabels[index][0], "predicted":predictedValue, "accuracy-trend":accuracy});
+        });
+        return this.accuracyReport
+    }
+
     computeKnn() {
 
         let k_array = this.features
@@ -82,47 +79,40 @@ class PredictCost {
             .concat(this.labels, 1) // concating with labels inoder to sort
             .unstack() // converting whole tensor to array of tensors
             .sort((a, b) => a.get(0) > b.get(0) ? 1 : -1) // sort by distance 
-            .slice(0,this.K) // selecting the top K records
-    
-    //Now we have the top K records of tensors, 
-        this.predictedNumber = _.chain(k_array)
-            .map(x => x.dataSync()) // takes data from tensor 
-            .countBy() // counts the the repitative records and assigns a rank, for example {"[2312,123]":3}
-            .toPairs() // converts array of objects to array of arrays
-            .sortBy(row => row[1]) // sorting by rank
-            .last() // taking the most frequent record
-            .first() // taking the key i.e., data by skipping the rank
-            .split(",") // since it is in string converting it to array
-            .last() // taking the cost value i.e, the second item of the array
-            .parseInt() // again converting the string to Integer
-            .value() // returning it
+            .slice(0, this.options.kvalue) // selecting the top K records
+
+        //Now we have the top K records of tensors,
+        if (this.options.strategy === "OCC") {
+            this.predictedNumber = _.chain(k_array)
+                .map(x => x.dataSync()) // takes data from tensor 
+                .countBy() // counts the the repitative records and assigns a rank, for example {"[2312,123]":3}
+                .toPairs() // converts array of objects to array of arrays
+                .sortBy(row => row[1]) // sorting by rank
+                .last() // taking the most frequent record
+                .first() // taking the key i.e., data by skipping the rank
+                .split(",") // since it is in string converting it to array
+                .last() // taking the cost value i.e, the second item of the array
+                .parseInt() // again converting the string to Integer
+                .value() // returning it
+        } else {
+            var Sum = _.chain(k_array)
+                .map(x => x.dataSync())
+                .reduce(function(sum, obj) {
+                    return sum + parseFloat(obj['1'])
+                }, 0)
+                .value();
+            this.predictedNumber = Sum / k_array.length
+
+        }
+        //Now we have the top K records of tensors, 
+
+
         return this.predictedNumber
     }
 
-    
-    
-  
+
+
+
 }
 
-
-
-var mydataset = [{"_id":"5c6a83e1310dd83f24aa1fj9","item_description":"window","material":"glass","damage_percentage":40,"cost_repair":10000,"location":"Jaipur","image_url":"","claimid":""},
-                {"_id":"5c6a83e1310dd83f24aa1fd9","item_description":"window","material":"glass","damage_percentage":40,"cost_repair":20000,"location":"Mumbai","image_url":"","claimid":""},
-                {"_id":"5c7a83e1310dd83f24aa1fd8","item_description":"window","material":"glass","damage_percentage":100,"cost_repair":100000,"location":"Mumbai","image_url":"","claimid":""},
-                {"_id":"5c8a83e1310dd83f24aa1fj9","item_description":"window","material":"glass","damage_percentage":40,"cost_repair":10000,"location":"Jaipur","image_url":"","claimid":""},
-                {"_id":"5c9a83e1310dd83f24aa1fd9","item_description":"pipe","material":"PVC","damage_percentage":50,"cost_repair":1000,"location":"Mumbai","image_url":"","claimid":""},
-                {"_id":"5c6a83e1310dd83f24aa1fd8","item_description":"roof","material":"concrete","damage_percentage":90,"cost_repair":500000,"location":"Mumbai","image_url":"","claimid":""},
-                {"_id":"5c6a83e1310dd83f24aa1fd9","item_description":"window","material":"glass","damage_percentage":40,"cost_repair":20000,"location":"Mumbai","image_url":"","claimid":""},
-                {"_id":"5c7a83e1310dd83f24aa1fd8","item_description":"window","material":"glass","damage_percentage":100,"cost_repair":100000,"location":"Mumbai","image_url":"","claimid":""},
-                {"_id":"5c8a83e1310dd83f24aa1fj9","item_description":"window","material":"glass","damage_percentage":40,"cost_repair":10000,"location":"Jaipur","image_url":"","claimid":""},
-                {"_id":"5c9a83e1310dd83f24aa1fd9","item_description":"pipe","material":"PVC","damage_percentage":50,"cost_repair":1000,"location":"Mumbai","image_url":"","claimid":""},
-                {"_id":"5c6a83e1310dd83f24aa1fd8","item_description":"roof","material":"concrete","damage_percentage":90,"cost_repair":500000,"location":"Mumbai","image_url":"","claimid":""}]
-
-var predictFor = {"_id":"5c6a83e1310dd83f24aa1fd9","item_description":"window","material":"glass","damage_percentage":50,"cost_repair":0,"location":"Mumbai","image_url":"","claimid":""}
-
-
-var prediction = new PredictCost(mydataset,predictFor)
-
-var cost = prediction.computeKnn();
-
-console.log("========> "+cost)
+module.exports = PredictCost;
